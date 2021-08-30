79ea7fe5792dc58e50c8480b2a31f6f2add3501d
jasonhtpham <htpham@deakin.edu.au>
2021-08-29 20:26:27 -0700

    implemnt ERC721 contract

--- contract/index.js
+++ contract/index.js
@@ -7,6 +7,10 @@
 'use strict';
 
 const certificateIssuer = require('./lib/certificateIssuer');
+const TokenERC721Contract = require('./lib/tokenERC721');
 
-module.exports.CertificateIssuer = certificateIssuer;
-module.exports.contracts = [certificateIssuer];
+// module.exports.CertificateIssuer = certificateIssuer;
+// module.exports.contracts = [certificateIssuer];
+
+module.exports.TokenERC721Contract = TokenERC721Contract;
+module.exports.contracts = [TokenERC721Contract];
